ebdf1b8bd192c40ba5b1ee9ed27ecbc5fad85c76
oron <oron0412@gmail.com>
2019-07-15 15:16:38 +0300

    hii

--- app/src/main/java/com/oron/restaurantrating/Activities/MainActivity.java
+++ app/src/main/java/com/oron/restaurantrating/Activities/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity implements
 
     FragmentManager fragmentManager = getSupportFragmentManager();
     Fragment fragment = null;
-
+    //hiii
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
