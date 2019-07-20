10216d33a7b8f4310ba5651aafb68ea3138cbfe0
oron <oron0412@gmail.com>
2019-07-20 18:27:34 +0300

    show list of Questions

--- app/src/main/java/com/oron/restaurantrating/Activities/FormActivity.java
+++ app/src/main/java/com/oron/restaurantrating/Activities/FormActivity.java
@@ -1,15 +1,116 @@
 package com.oron.restaurantrating.Activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.oron.restaurantrating.Data.QuestionRecyclerAdapter;
+import com.oron.restaurantrating.Model.QuestionView;
 import com.oron.restaurantrating.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FormActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private QuestionRecyclerAdapter questionRecyclerAdapter;
+    private List<QuestionView> questionViewList;
+
+    private DatabaseReference myDatabaseReference;
+    private FirebaseDatabase myDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        myDatabase = FirebaseDatabase.getInstance();
+        myDatabaseReference = myDatabase.getReference().child("Question");
+        myDatabaseReference.keepSynced(true);
+
+        questionViewList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recycleViewForm);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.user_account_menu, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//        switch (item.getItemId()) {
+//            case R.id.action_add:
+//                if (mAuth != null && mUser != null) {
+//                    startActivity(new Intent(PostListActivity.this, AddPostActivity.class));
+//                    finish();
+//                }
+//               break;
+//            case R.id.action_signout:
+//                if (mAuth != null && mUser != null) {
+//                    mAuth.signOut();
+//                    startActivity(new Intent(PostListActivity.this, MainActivity.class));
+//                    finish();
+//                }
+//                break;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        myDatabaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                QuestionView questionView = dataSnapshot.getValue(QuestionView.class);
+
+                questionViewList.add(questionView);
+
+                questionRecyclerAdapter = new QuestionRecyclerAdapter(FormActivity.this, questionViewList);
+                recyclerView.setAdapter(questionRecyclerAdapter);
+                questionRecyclerAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/oron/restaurantrating/Data/QuestionRecyclerAdapter.java
+++ app/src/main/java/com/oron/restaurantrating/Data/QuestionRecyclerAdapter.java
@@ -3,10 +3,14 @@ package com.oron.restaurantrating.Data;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.oron.restaurantrating.Model.QuestionView;
+import com.oron.restaurantrating.R;
 
 import java.util.List;
 
@@ -14,6 +18,8 @@ public class QuestionRecyclerAdapter extends RecyclerView.Adapter<QuestionRecycl
 
     private Context context;
     private List<QuestionView> questionList;
+
+
 
     public QuestionRecyclerAdapter(Context context, List<QuestionView> questionList) {
         this.context = context;
@@ -23,22 +29,39 @@ public class QuestionRecyclerAdapter extends RecyclerView.Adapter<QuestionRecycl
     @NonNull
     @Override
     public QuestionRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.questions_row, viewGroup, false);
+        return new ViewHolder(view, context);
     }
 
     @Override
     public void onBindViewHolder(@NonNull QuestionRecyclerAdapter.ViewHolder viewHolder, int i) {
+        QuestionView questionView = questionList.get(i);
+
+        viewHolder.question.setText(questionView.getQuestion());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return questionList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public TextView question;
+
+        public ViewHolder(@NonNull View view, Context ctx) {
+            super(view);
+            context = ctx;
+
+            question = view.findViewById(R.id.questinTextView);
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, "item click", Toast.LENGTH_SHORT).show();
+
+                }
+            });
         }
     }
 }
