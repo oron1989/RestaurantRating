39cacfee1a935e71dc9ad3d32ebaf6875efe93b4
oron <oron0412@gmail.com>
2019-07-21 13:29:28 +0300

    add popup

--- app/src/main/java/com/oron/restaurantrating/UI/QuestionRecyclerAdapter.java
+++ app/src/main/java/com/oron/restaurantrating/UI/QuestionRecyclerAdapter.java
@@ -2,10 +2,14 @@ package com.oron.restaurantrating.UI;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.oron.restaurantrating.Model.QuestionView;
@@ -18,7 +22,9 @@ public class QuestionRecyclerAdapter extends RecyclerView.Adapter<QuestionRecycl
     private Context context;
     private List<QuestionView> questionList;
 
-
+    private AlertDialog.Builder alertDialogBuilder;
+    private AlertDialog dialog;
+    private LayoutInflater inflater;
 
     public QuestionRecyclerAdapter(Context context, List<QuestionView> questionList) {
         this.context = context;
@@ -62,8 +68,50 @@ public class QuestionRecyclerAdapter extends RecyclerView.Adapter<QuestionRecycl
         public void onClick(View v) {
             //Get position of the row clicked or tapped
             int position = getAdapterPosition();
-
             QuestionView question = questionList.get(position);
+
+            editItem(question);
+
         }
+
+        public void editItem(QuestionView question) {
+            alertDialogBuilder = new AlertDialog.Builder(context);
+            inflater = LayoutInflater.from(context);
+
+            final View view = inflater.inflate(R.layout.popup_question, null);
+
+            TextView questionText = view.findViewById(R.id.textAlert);
+            RadioGroup radioGroup = view.findViewById(R.id.radioGroup);
+            RadioButton answer1 = view.findViewById(R.id.radioButtonOption1);
+            RadioButton answer2 = view.findViewById(R.id.radioButtonOption2);
+            RadioButton answer3 = view.findViewById(R.id.radioButtonOption3);
+            RadioButton answer4 = view.findViewById(R.id.radioButtonOption4);
+            RadioButton answer5 = view.findViewById(R.id.radioButtonOption5);
+            Button okButton = view.findViewById(R.id.okButton);
+            Button cancelButton = view.findViewById(R.id.cancelButton);
+
+            questionText.setText(question.getQuestion());
+
+            alertDialogBuilder.setView(view);
+            dialog = alertDialogBuilder.create();
+            dialog.show();
+
+            okButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+            cancelButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog.dismiss();
+                }
+            });
+
+
+        }
+
     }
 }
